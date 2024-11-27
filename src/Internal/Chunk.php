35540da7d927178a47c197b1d3a4e0fd64db14b3
ging-dev <thanh1101dev@gmail.com>
2024-11-27 20:22:06 +0700

    chore: add `dot`

--- src/Internal/Chunk.php
+++ src/Internal/Chunk.php
@@ -10,6 +10,8 @@ use League\HTMLToMarkdown\HtmlConverter;
  */
 final class Chunk
 {
+    private const END = '2.1.4.4';
+
     private function __construct(
         public string $content,
         public bool $stop,
@@ -21,15 +23,30 @@ final class Chunk
         $data = json_decode($message->buffer(), true);
         $diff = array_pop($data);
         $content = '';
-        $stop = isset($diff[2][1][4][4]);
-        if ($chunk = $diff['e'][0][1]['data'] ?? false) {
+        $stop = false !== self::dot(self::END, $diff);
+        if ($chunk = self::dot('e.0.1.data', $diff)) {
             $content = str_replace('<br/>', PHP_EOL, $chunk);
         }
-        if ($cache = $diff['response']['rendered'][2][1][4][4] ?? false) {
+        if ($cache = self::dot('response.rendered.'.self::END, $diff)) {
             $content = (new HtmlConverter())->convert($cache);
             $stop = true;
         }
 
         return new self($content, $stop);
+    }
+
+    /**
+     * @param mixed[] $value
+     */
+    private static function dot(string $keys, array $value): string|false
+    {
+        foreach (explode('.', $keys) as $key) {
+            if (!array_key_exists($key, $value)) {
+                return false;
+            }
+            $value = $value[$key];
+        }
+
+        return $value;
     }
 }
